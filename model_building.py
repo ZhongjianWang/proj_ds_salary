8a9b72c2e8f3f81c6e3786f5ba6885849c365bba
王中坚 <zhongjian.wang@stud.uni-due.de>
2020-10-01 12:41:24 +0200

    upload graphs

---
+++ model_building.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 31 10:29:34 2020
+
+@author: jason
+"""
+import pandas as pd
+import matplotlib as plt
+import numpy as np
+
+df= pd.read_csv('eda_data.csv')
